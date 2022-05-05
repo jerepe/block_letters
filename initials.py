91db6442f1d17b8073e4a64c367ce52e6e2fcd1a
jeremy <74825770+jerepe@users.noreply.github.com>
2022-05-05 12:55:37 +0200

    first commit

---
+++ initials.py
@@ -0,0 +1,10 @@
+# My name is Jeremy Perreau
+# Let's connect !
+
+print("JJJJJJJ      PPPPPP ")
+print("   J         P     P")
+print("   J         P     P")
+print("   J         PPPPPP ")
+print("J  J         P      ")
+print("J  J         P      ")
+print(" JJJ         P      ")
